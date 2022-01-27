9a0c508eb1ccd775a55abbfa9545135dedb6d8f8
Abdelkodouss Belhaimer <abdelkodoussbelhaimerbusiness@gmail.com>
2022-01-27 11:43:14 +0100

    homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import './App.css';
+import HomePage from './homePage.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
